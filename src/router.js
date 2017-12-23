c2a12dd0554355223bd83a943857e81b54ce42be
SalamDalloul <32265731+Salam-Dalloul@users.noreply.github.com>
2017-12-23 21:07:14 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -3,12 +3,16 @@ const handlers = require('./handler');
 const router = (request, response) => {
   const endpoint = request.url.split('/')[1];
   if (endpoint === '') {
-handlers.handleHomePages(request, response);
+    handlers.handleHomePages(request, response);
   } else if (endpoint === 'sign-up') {
     handlers.handleSignUp(request, response);
-  } else if (endpoint === 'signup' && request.method==='POST') {
-      handlers.handleaddNewUser(request, response);
-    } else {
+  } else if (endpoint === 'signup' && request.method === 'POST') {
+    handlers.handleaddNewUser(request, response);
+  } else if (endpoint === 'login' && request.method === 'GET') {
+    handlers.handleLogIn(request, response);
+  } else if (endpoint === 'todo') {
+    handlers.handleToDoPage(request, response);
+  } else {
     handlers.handleGeneric(request, response);
   }
 };
